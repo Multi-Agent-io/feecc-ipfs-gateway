8a036e9cc844db3b6f9c27c149a193315f20fb2c
arseniiarsenii <arseniivelichko2@gmail.com>
2022-05-25 21:16:35 +0300

    updated robonomics interface

--- src/io_gateway/robonomics.py
+++ src/io_gateway/robonomics.py
@@ -1,23 +1,24 @@
 import typing as tp
 
 from loguru import logger
-from robonomicsinterface import RobonomicsInterface
+from robonomicsinterface import Account, Datalog
 from typed_getenv import getenv
 
 DATALOG_ENABLED: bool = getenv("ROBONOMICS_ENABLE_DATALOG", var_type=bool, default=False)
-ROBONOMICS_ACCOUNT_SEED: str = getenv("ROBONOMICS_ACCOUNT_SEED", var_type=str, default="", optional=True)
-ROBONOMICS_CLIENT: tp.Optional[RobonomicsInterface] = None
+ROBONOMICS_ACCOUNT: tp.Optional[Account] = None
+DATALOG_CLIENT: tp.Optional[Datalog] = None
 
 if DATALOG_ENABLED:
-    assert ROBONOMICS_ACCOUNT_SEED, "Datalog is enabled, but the seed is missing. Export it to ROBONOMICS_ACCOUNT_SEED."
-    ROBONOMICS_CLIENT = RobonomicsInterface(
-        seed=ROBONOMICS_ACCOUNT_SEED, remote_ws=getenv("ROBONOMICS_SUBSTRATE_NODE_URL", var_type=str, default=None)
+    ROBONOMICS_ACCOUNT = Account(seed=getenv("ROBONOMICS_ACCOUNT_SEED", var_type=str))
+    DATALOG_CLIENT = Datalog(
+        account=ROBONOMICS_ACCOUNT,
+        wait_for_inclusion=False,
     )
 
 
 def post_to_datalog(content: str) -> None:
     """echo provided string to the Robonomics datalog"""
     logger.info(f"Posting data '{content}' to Robonomics datalog")
-    assert ROBONOMICS_CLIENT
-    txn_hash: str = ROBONOMICS_CLIENT.record_datalog(content)
+    assert DATALOG_CLIENT
+    txn_hash: str = DATALOG_CLIENT.record(content)
     logger.info(f"Data '{content}' has been posted to the Robonomics datalog. {txn_hash=}")
