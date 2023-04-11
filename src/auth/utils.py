ef1e7807ba42a65cfbcafd4c7aeef160b63d80c5
PaTara <p040399@outlook.com>
2023-04-12 00:40:41 +0300

    updated authentication logic, removed datalog posting

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -3,19 +3,13 @@ from typing import Any
 from fastapi import Depends
 from typed_getenv import getenv
 
-from auth.dependencies import authenticate_analytics, authenticate_workbench
+from auth.dependencies import authenticate
 
-AUTHENTICATION_MODE: str = getenv("AUTH_MODE", var_type=str)
+AUTHENTICATE: bool = getenv("AUTHENTICATE", var_type=bool)
 
 
 def load_auth_mode() -> list[Any] | None:
-    match AUTHENTICATION_MODE:
-        case "noauth":
-            return None
-        case "workbench":
-            AUTH_DEPENDENCY = [Depends(authenticate_workbench)]
-        case "analytics":
-            AUTH_DEPENDENCY = [Depends(authenticate_analytics)]
-        case _:
-            raise ValueError(f"Unknown authentication mode: {AUTHENTICATION_MODE}")
-    return AUTH_DEPENDENCY
+    if AUTHENTICATE:
+        return [Depends(authenticate)]
+    else:
+        return None
